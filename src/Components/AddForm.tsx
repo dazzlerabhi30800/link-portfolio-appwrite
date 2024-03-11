71a9a88ceaadecb1077c2fae99c1655af0ffc896
= <abhisingh30800@gmail.com>
2024-03-11 19:13:06 +0530

    added form component

---
+++ src/Components/AddForm.tsx
@@ -0,0 +1,54 @@
+import { Dispatch, FormEvent, SetStateAction } from "react";
+
+type addForm = {
+  setAdd: Dispatch<SetStateAction<boolean>>;
+};
+export default function AddForm({ setAdd }: addForm) {
+  const handleSubmit = (event: Event | FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(
+      document.querySelector("form") as HTMLFormElement,
+    );
+    let title = formData.get("title");
+    let link = formData.get("link");
+    console.log({ title, link });
+    setAdd(false);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-10 p-5 rounded-md shadow-md w-[95%] max-w-[350px] bg-neutral-900"
+    >
+      <div className="flex flex-col gap-2">
+        <label htmlFor="title" className="md:text-lg">
+          Title
+        </label>
+        <input
+          type="text"
+          className="border border-sky-400 rounded-md py-2 px-4 bg-transparent text-white focus:outline-none focus:border-rose-400"
+          name="title"
+          placeholder="Title"
+          id="title"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="link" className="md:text-lg">
+          Link URL
+        </label>
+        <input
+          type="text"
+          className="border border-sky-400 rounded-md py-2 px-4 bg-transparent text-white focus:outline-none focus:border-rose-400"
+          name="link"
+          placeholder="Link"
+          id="link"
+        />
+      </div>
+      <button
+        type="submit"
+        className="py-2 px-6 shadow-lg bg-rose-500 transition ease-in-out duration-300 hover:bg-rose-700"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
--- src/app/links/[link]/page.tsx
+++ src/app/links/[link]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import "../../globals.css";
+import { FaPlus } from "react-icons/fa";
+import { useState } from "react";
+import AddForm from "@/Components/AddForm";
 
 interface params {
   params: {
@@ -7,12 +10,21 @@ interface params {
   };
 }
 export default function LinkPage({ params: { link } }: params) {
-  console.log(link);
+  const [add, setAdd] = useState<boolean>(false);
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex flex-col gap-8 min-h-screen items-center justify-center">
       <h1 className="text-3xl md:text-[2.6rem] font-medium uppercase text-cyan-30 bg-gradient-to-r from-purple-500 via-red-500 to-cyan-400 animate-bgchange">
         {link} Links
       </h1>
+      {!add && (
+        <button
+          className="p-3 border-2 border-rose-600 text-rose-600 transition linear duration-300 hover:text-cyan-500 hover:border-cyan-500 rounded-md"
+          onClick={() => setAdd((prev) => !prev)}
+        >
+          <FaPlus />
+        </button>
+      )}
+      {add && <AddForm setAdd={setAdd} />}
     </main>
   );
 }
